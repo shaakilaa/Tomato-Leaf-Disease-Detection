8963e11aafde78f37e99342b40af41688a3f013a
Shakila <shakilaa2001hashemi@gmail.com>
2026-06-15 21:14:15 +0330

    import commit

---
+++ train.py
@@ -0,0 +1,5 @@
+import pathlib
+from tensorflow import keras # type: ignore
+from tensorflow.keras import layers # type: ignore
+from tensorflow.keras.utils import image_dataset_from_directory # type: ignore
+import matplotlib.pyplot as plt
