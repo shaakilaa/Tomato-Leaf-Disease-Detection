b231ee89217c8e0abe4e79a1fbbcdc34e70963d9
Shakila <shakilaa2001hashemi@gmail.com>
2026-06-16 14:39:33 +0330

    building the model architecture and summary

--- train.py
+++ train.py
@@ -3,3 +3,22 @@ from tensorflow import keras # type: ignore
 from tensorflow.keras import layers # type: ignore
 from tensorflow.keras.utils import image_dataset_from_directory # type: ignore
 import matplotlib.pyplot as plt
+
+dir = pathlib.Path("tomato-leaf-dataset")
+
+inputs = keras.Input(shape=(256, 256, 3))
+x = layers.Rescaling(1./255)(inputs)
+x = layers.Conv2D(filters=32, kernel_size=3, activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=2)(x)
+x = layers.Conv2D(filters=64, kernel_size=3, activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=2)(x)
+x = layers.Conv2D(filters=128, kernel_size=3, activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=2)(x)
+x = layers.Conv2D(filters=256, kernel_size=3, activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=2)(x)
+x = layers.Conv2D(filters=256, kernel_size=3, activation="relu")(x)
+x = layers.Flatten()(x)
+outputs = layers.Dense(10, activation="softmax")(x)
+model = keras.Model(inputs=inputs, outputs=outputs)
+
+model.summary()
